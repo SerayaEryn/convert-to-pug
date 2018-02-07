cbd7c0c22c23cbb0043f43cf927e56642450bd10
Denis Fäcke <serayaeryn@gmail.com>
2018-02-07 20:21:39 +0100

    fix attribute interpolation conversion

--- test/attribute-interpolation.test.js
+++ test/attribute-interpolation.test.js
@@ -4,7 +4,7 @@ const t = require('tap');
 const test = t.test;
 const converter = require('../lib/converter')
 
-test('', t => {
+test('Should convert attribute', t => {
   t.plan(1);
   const code = 'a(href="before#{link}after", test=\'test\')';
 
@@ -22,7 +22,7 @@ test('Should convert intended attribute', t => {
   t.strictEqual(resultCode, 'div\n  a(href=`before${link}after`, test=\'test\')');
 });
 
-test('', t => {
+test('Should convert attributes', t => {
   t.plan(1);
   const code = 'a(href=\'before#{link}after\', test=\'test\')';
 
@@ -31,7 +31,16 @@ test('', t => {
   t.strictEqual(resultCode, 'a(href=`before${link}after`, test=\'test\')');
 });
 
-test('', t => {
+test('Should convert attributes without comma', t => {
+  t.plan(1);
+  const code = 'a(href=\'before#{link}after\' test=\'test\')';
+
+  const resultCode = converter.convert(code);
+
+  t.strictEqual(resultCode, 'a(href=`before${link}after` test=\'test\')');
+});
+
+test('Should only convert attribute interpolation', t => {
   t.plan(1);
   const code = 'a(href="before${link}after", test=\'test\')';
 
@@ -39,3 +48,40 @@ test('', t => {
 
   t.strictEqual(resultCode, 'a(href="before${link}after", test=\'test\')');
 });
+
+test('Should not replace quotation marks inside attribute interpolation', t => {
+  t.plan(1);
+  const code = 'a(href="before#{link + \'test\'}after", test=\'test\')';
+
+  const resultCode = converter.convert(code);
+
+  t.strictEqual(resultCode, 'a(href=`before${link + \'test\'}after`, test=\'test\')');
+});
+
+test('Should handle concatination correctly', t => {
+  t.plan(1);
+  const code = 'a(href="before#{link + \'test\'}after" + test, test=\'test\')';
+
+  const resultCode = converter.convert(code);
+
+  t.strictEqual(resultCode, 'a(href=`before${link + \'test\'}after` + test, test=\'test\')');
+});
+
+test('Should handle concatination correctly', t => {
+  t.plan(1);
+  const code = 'a(href="#{test}" + "#{test}")';
+
+  const resultCode = converter.convert(code);
+
+  t.strictEqual(resultCode, 'a(href=`${test}` + `${test}`)');
+});
+
+test('Should handle concatination correctly', t => {
+  t.plan(1);
+  const code = 'a(href="#{test}" + \'#{test}\')';
+
+  const resultCode = converter.convert(code);
+
+  t.strictEqual(resultCode, 'a(href=`${test}` + `${test}`)');
+});
+
